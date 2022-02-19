8645ed2080a60444e28cbbe4d2442e6daf2cf520
94ek786 <84435176+94ek786@users.noreply.github.com>
2022-02-19 13:05:48 +0800

    65%

--- imnotrobot.js
+++ imnotrobot.js
@@ -1,6 +1,6 @@
-var num = '1';
-var ver;
-var ans;
+var INR_num = '1';
+var INR_ver;
+var INR_ans;
 
 function INR_notpass() {
     document.getElementById('imnotrobot').innerHTML = '\
@@ -35,53 +35,54 @@ function INR_pass() {
 };
 
 function INR_start() {
-    ver = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    INR_ans = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+    INR_ver = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     document.getElementById('imnotrobot').innerHTML = '\
-        <table style="border:3px black solid;">\
+        <table style="border:3px black solid;background-color: #f0f0f0;">\
             <thead>\
                 <tr>\
-                    <th colspan="4"><div style="background-image: url(img/' + num + '/top.jpg);background-size: contain;background-repeat: no-repeat;width: 200px;height: 60px;"></div></td>\
+                    <th colspan="4"><div style="background-image: url(img/' + INR_num + '/top.jpg);background-size: contain;background-repeat: no-repeat;width: 200px;height: 60px;"></div></td>\
                 </tr>\
             </thead>\
             <tbody>\
                 <tr>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(0)">\
-                        <div style="background-image: url(img/' + num + '/0.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img0"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/0.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img0"></div>\
                     </td>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(1)">\
-                        <div style="background-image: url(img/' + num + '/1.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img1"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/1.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img1"></div>\
                     </td>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(2)">\
-                        <div style="background-image: url(img/' + num + '/2.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img2"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/2.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img2"></div>\
                     </td>\
                 </tr>\
                 <tr>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(3)">\
-                        <div style="background-image: url(img/' + num + '/3.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img3"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/3.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img3"></div>\
                     </td>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(4)">\
-                        <div style="background-image: url(img/' + num + '/4.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img4"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/4.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img4"></div>\
                     </td>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(5)">\
-                        <div style="background-image: url(img/' + num + '/5.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img5"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/5.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img5"></div>\
                     </td>\
                 </tr>\
                 <tr>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(6)">\
-                        <div style="background-image: url(img/' + num + '/6.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img6"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/6.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img6"></div>\
                     </td>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(7)">\
-                        <div style="background-image: url(img/' + num + '/7.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img7"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/7.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img7"></div>\
                     </td>\
                     <td width="60" height="60" style="cursor: pointer;" onclick="INR_pick(8)">\
-                        <div style="background-image: url(img/' + num + '/8.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img8"></div>\
+                        <div style="background-image: url(img/' + INR_num + '/8.jpg);background-size: contain;background-repeat: no-repeat;width: 60px;height: 60px;" id="INR_img8"></div>\
                     </td>\
                 </tr>\
             </tbody>\
             <tfoot>\
                 <tr>\
                     <th style="cursor: pointer;" colspan="3">\
-                        <div style="float:right;color: white;width: 70px;height: 40px;border-radius: 5px;background: rgba(76, 144, 224);line-height:40px;" onclick="INR_verification()">略過</div>\
+                        <div id="INR_verify" style="float:right;color: white;width: 70px;height: 40px;border-radius: 5px;background: rgba(76, 144, 224);line-height:40px;" onclick="INR_verification()">略過</div>\
                     </th>\
                 </tr>\
             </tfoot>\
@@ -89,18 +90,20 @@ function INR_start() {
 };
 
 function INR_pick(n) {
-    if (ver[n] == 0) {
+    if (INR_ver[n] == 0) {
         document.getElementById('INR_img' + n).innerHTML = '<div style="width: 20px;height: 20px;text-align: center;border: 1px solid rgba(173, 216, 230.2);background-color: rgba(173, 216, 230.2);opacity: .9;border-radius: 50%;color: white;">✔</div>';
-        ver[n] = 1;
+        INR_ver[n] = 1;
     } else {
         document.getElementById('INR_img' + n).innerHTML = '';
-        ver[n] = 0;
+        INR_ver[n] = 0;
+        return;
     };
+    document.getElementById('INR_verify').innerHTML = '驗證';
 };
 
 function INR_isans() {
     for (loop = 0; loop < 9; loop++) {
-        if (ver[loop] !== ans[loop]) {
+        if (INR_ver[loop] !== INR_ans[loop]) {
             return false;
         };
     };
@@ -117,7 +120,6 @@ function INR_verification() {
 
 function INR_load() {
     INR_notpass();
-    ans = [1, 1, 1, 1, 1, 1, 1, 1, 1];
 };
 
 window.onload = function() {
